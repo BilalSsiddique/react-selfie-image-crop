c64d234041369a1d513ef5168336add4dbc0eb35
BilalSsiddique <bsiddiqui@students.uit.edu>
2023-03-12 21:14:07 +0500

    updated

--- src/CaptureScreen.jsx
+++ src/CaptureScreen.jsx
@@ -165,7 +165,7 @@ const CaptureCard = (props) => {
             keepSelection={false}
             className="white-transparent"
             style={{
-              width: '50%',
+              width: '100%',
               height: '100%',
               overflow:'hidden',
               position: 'absolute',
